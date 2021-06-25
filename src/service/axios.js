289791739ede05ce0b4c86dfbdf3e38ff9fb04a1
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-06-25 14:59:02 +0530

    add : connecting with backend using axios package

---
+++ src/service/axios.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+axios.defaults.baseURL=process.env.VUE_APP_ROOT_URL
+axios.defaults.headers.common['Authorization']='Bearer'+ localStorage.getItem('token');
+
+export default class AxiosService{
+    postData(url, data){
+        return axios.post(url, data).then(response =>{
+            return response;
+        }).catch(error =>{
+            return error;
+        })
+    }
+}
