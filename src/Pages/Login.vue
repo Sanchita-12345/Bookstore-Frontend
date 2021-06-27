d9f5bc8574937d1593239d305bf0ca9c27fcce56
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-06-27 21:19:42 +0530

    modify : alert message given properly

---
+++ src/Pages/Login.vue
@@ -0,0 +1,65 @@
+<template>
+<div class="main">
+    <div class="container">
+        <img src="../assets/picture.png" id="side-img" alt="notFound" />
+        <p>Online Book Shopping</p>
+        <div class="box">
+            <div class="headings">
+                <h5 class="signin" :class="{ active: isVisible }" @click="isVisible = true" id="sign-in">Login</h5>
+                <h5 class="signup" :class="{ active: !isVisible }" @click="isVisible = false" id="sign-up">signup</h5>
+            </div>
+            <form ref="myForm" @submit.prevent="handleSubmit">
+                <div class="username">
+                    <p>EmailID</p>
+                    <input type="email" class="emailbox" required v-model="email" id="email-id" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$">
+                </div>
+                <div class="password-section">
+                    <p>Password</p>
+                    <input :type="passwordType" class="password" id="pwd" v-model="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$">
+                    <img src="../../src/assets/show-hide-icon-28.jpg" onclick="show();" id="eye" @click="togglePasswordButton()">
+                </div>
+                <a href="">forgot password?</a>
+                <button class="btn-section" id="login-btn" type="submit">Login</button>
+            </form>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import service from '../service/User'
+export default{
+    name: 'Login',
+    data() {
+        return{
+            email: '',
+            password: '',
+            passwordType: "password",
+            isVisible: true
+        }
+    },
+    methods: {
+        togglePasswordButton() {
+            this.passwordType = this.passwordType === 'password' ? 'text' : 'password'
+        },
+        async handleSubmit(){
+            let userData = {
+                email: this.email,
+                password: this.password
+            }
+            service.userLogin(userData).then(response =>{
+                localStorage.getItem('token', response.data.token);
+                alert("logged in..");
+                this.$refs.myForm.reset();
+            }).catch(error =>{
+                alert("error...!!! invalid input");
+                return error;
+            })
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/Styles/Login.scss";
+</style>
