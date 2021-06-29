31be6f42b8ff21b81894dbc42efddd1d604e38d1
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-06-29 21:20:04 +0530

    add : after successfully getting the mail for forgot password the page will redirected to the reset password page

--- src/Pages/Forgot.vue
+++ src/Pages/Forgot.vue
@@ -32,7 +32,7 @@ export default {
             service.userForgotPassword(userData).then(response => {
                 console.log(response);
                 alert("mail is sended successfully")
-                // this.$router.push('/resetPassword/:resetToken')
+                this.$router.push('/resetPassword/:resetToken')
             }).catch(error =>{
                 alert("given email id is not registered...!!!");
                 return error;
