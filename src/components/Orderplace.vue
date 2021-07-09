e0cbb2c80144b61bf28734206f69dc24409ebb98
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-07-09 22:55:19 +0530

    build : Order placed successfully page designed

---
+++ src/components/Orderplace.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="order-place">
+    <div class="image-container">
+        <img src="../assets/Group 4132.png" alt="not found"/>
+        
+    </div>
+    <div class="title-container">
+        <p>Order placed Successfully</p>
+    </div>
+    <div class="message-section">
+        <p>hurray!!!your order is confirmed the order id is #123456 
+            save the order id for further communication..</p>
+    </div>
+    <div class="title-section">
+        <div class="email-us">
+            <p>Email-us</p>
+        </div>
+        <div class="contact-us">
+            <p>Contact-us</p>
+        </div>
+        <div class="address">
+            <p>Address</p>
+        </div>
+    </div>
+    <div class="email-sec">
+        <p>admin@bookstore.com</p>
+    </div>
+    <div class="contact-sec">
+        <p>+918163475881</p>
+    </div>
+    <div class="address-sec">
+        42, 14th Main, 15th Cross, Sector 4 ,opp to BDA complex,
+         near Kumarakom restaurant, HSR Layout, Bangalore 560034
+    </div>
+    <button class="continue-shopping"><a href="http://localhost:8080/dashboard">CONTINUE SHOPPING</a></button>
+</div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/Styles/Orderplace.scss";
+</style>
