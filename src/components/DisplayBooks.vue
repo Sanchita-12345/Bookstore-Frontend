e6778da6153bd887f0a32ab2c01f60064e6b1c82
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-07-17 10:33:43 +0530

    add : added to bag button added

--- src/components/DisplayBooks.vue
+++ src/components/DisplayBooks.vue
@@ -1,59 +1,103 @@
 <template>
-<div class="bookdisplay-section">
-    <div class="book" v-for="book in books" :key="book.id">
+<div class="carddisplay-section">
+    <div class="books-total">
+        <h6>Books<span class="items">({{books.length}}items)</span></h6>
+    </div>
+    <div v-for="book in books" :key="book.id" class="card-book">
         <div class="image-section">
             <div class="image-container">
-                <img v-bind:src="book.file" />
-                <!-- {{book.file}} -->
+                <img v-bind:src="book.file" style="height:135px;width:105px;" />
             </div>
         </div>
-        <div class="name-section">
+        <div class="title-section">
             {{book.name}}
         </div>
         <div class="author-section">
             by {{book.author}}
         </div>
         <div class="price-section">
-            Rs. {{book.price}}<label>(2000)</label>
-            <button v-if="flag" type="submit" @click="handleSubmit();Togglebutton();">close</button>
+            Rs. {{book.price}}
         </div>
-        <div class="rating">
-            <p>4.5<i class="fas fa-star"></i></p>
-        </div>
-        <div class="quantity">
-            <p>(20)</p>
+        <div class="buttons">
+            <div class="button-groups" v-if="!addedBooks.includes(book.id)">
+                <button type="submit" @click="handleCart(book.id);toggle(book.id);addedBooks.push(book.id)" class="AddBag">Add to Bag</button>
+                <button class="wishlist">wishlist</button>
+            </div>
+            <div class="AddedBag" v-else>
+                <button class="big-btn" @click="removeFromCart(book.id);addedBooks=addedBooks.filter(id=>id!==book.id)">Added to Bag</button>
+            </div>
         </div>
     </div>
 </div>
 </template>
+
 <script>
 import service from '../service/User'
-export default{
-    name: 'DisplayBooks',
-    data(){
-        return{
+export default {
+    mounted() {
+        service.userDisplayBooks().then(response => {
+            this.books.push(...response.data);
+            return response;
+        })
+    },
+    data() {
+        return {
+            isActive: true,
+            result: 0,
+            authorPrefix: 'by',
+            pricePrefix: 'Rs.',
+            defaultStrikePrice: '(2000)',
+            buttonValue: 'close',
             flag: true,
-            books: [{
-                id: 1,
-                file: 'display image',
-                name: 'Dont make me Think',
-                author : 'Joseph',
-                price : '1500',
-            },]
+            state: true,
+            addedBooks: [],
+            clickedCard: '',
+            books: []
         }
     },
-    methods:{
-        Togglebutton(){
+    watch: {
+        addedBooks: {
+            handler(val) {
+                this.$emit('update-books-count', val.length)
+            },
+            deep: true
+        }
+    },
+    methods: {
+        toggleClass: function (event) {
+            this.isActive = !this.isActive;
+            return event;
+        },
+        toggle(id) {
+            this.clickedCard = id;
+            console.log(this.clickedCard);
+        },
+        flip() {
+            this.state = !this.state;
+        },
+        Togglebtn() {
             this.flag = !this.flag;
         },
-        async handleSubmit(){
-            service.userDisplayBooks().then(response => {
-                this.books.push(...response.data);
+        handleCart(bookId) {
+            let userData = {
+                id: bookId,
+            }
+            service.userUpdateCart(userData).then(response => {
+                return response;
+            })
+        },
+        removeFromCart(bookId) {
+            let userData = {
+                id: bookId,
+            }
+            service.userRemoveFromCart(userData).then(response => {
+                return response;
             })
         },
     }
 }
 </script>
+
 <style lang="scss" scoped>
 @import "@/Styles/DisplayBooks.scss";
 </style>
