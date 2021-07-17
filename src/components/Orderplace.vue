fc4142fecd0a6363d134dd3284cfeae90584f650
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-07-17 10:36:16 +0530

    add : ordernumber added in front page after successfully order is placed

--- src/components/Orderplace.vue
+++ src/components/Orderplace.vue
@@ -1,15 +1,15 @@
 <template>
 <div class="order-place">
     <div class="image-container">
-        <img src="../assets/Group4132.png" alt="not found"/>
-        
+        <img src="../assets/Group4132.png" alt="not found" />
     </div>
     <div class="title-container">
         <p>Order placed Successfully</p>
     </div>
     <div class="message-section">
-        <p>hurray!!!your order is confirmed and placed successfully
-            contact us in below details for further communication..</p>
+        <p>hurray!!!your order is confirmed the order id is #{{orderNumber}}
+            save the order id for further communication
+        </p>
     </div>
     <div class="title-section">
         <div class="email-us">
@@ -30,11 +30,28 @@
     </div>
     <div class="address-sec">
         42, 14th Main, 15th Cross, Sector 4 ,opp to BDA complex,
-         near Kumarakom restaurant, HSR Layout, Bangalore 560034
+        near Kumarakom restaurant, HSR Layout, Bangalore 560034
     </div>
-    <button class="continue-shopping"><a href="http://localhost:8080/dashboard">CONTINUE SHOPPING</a></button>
+    <router-link class="continue-shopping" to="/dashboard" tag="button">CONTINUE SHOPPING</router-link>
 </div>
 </template>
+
+<script>
+import service from '../service/User'
+export default {
+    data() {
+        return {
+            orderNumber: '',
+        }
+    },
+    created() {
+        service.mailSendSuccessfully().then(response => {
+            console.log(typeof (this.orderNumber));
+            (this.orderNumber = JSON.stringify(response.data))
+        })
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 @import "@/Styles/Orderplace.scss";
